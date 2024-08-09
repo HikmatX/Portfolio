e796ece3c265e38700143324dfa6205200c5bcb8
Hikmatbek-dev <hikmatcoder@gmail.com>
2024-08-09 10:42:19 +0500

    Portfolio

---
+++ add.py
@@ -0,0 +1,7 @@
+a = """
+Serquyosh hur o'lkam, elga baxt, najot, 
+Sen o'zing do'stlarga yo'ldosh, mehribon! 
+Yashnagay to abad ilmu fan, ijod, 
+Shuhrating porlasin toki bor jahon!
+"""
+print(a[0:9])
